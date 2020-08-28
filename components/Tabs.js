1f032c9589db6a6bc171bc1c449ccca65b98d695
mrjoshuamartinez <mrjoshuamartinez@gmail.com>
2020-08-28 04:47:44 -0400

    Created axios GET request, console.log for verification of proper function

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,12 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+axios
+.get(`https://lambda-times-api.herokuapp.com/topics`)
+.then((res) => {
+    console.log(res);
+    })
+.catch((err) => {
+    console.log(err);
+});
