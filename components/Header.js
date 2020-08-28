ad6baf19cf551c68b50931455af5d5ee91938298
mrjoshuamartinez <mrjoshuamartinez@gmail.com>
2020-08-28 04:34:57 -0400

    Created Elements

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,12 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // creating elements
+    const divTag = document.createElement ('div');
+    const date = document.createElement ('span');
+    const h1Tag = document.createElement ('h1');
+    const temp = document.createElement ('span');
+}
+
+Header ();
